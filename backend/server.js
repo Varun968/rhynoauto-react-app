5ae02fd8ff8703b49b1abb2e5a317095fb751465
Varun968 <vsruparelia241@gmail.com>
2024-08-07 19:08:22 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/rhynoauto', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
+// Connect to the database
+connectDB();
 
 // Import and use routes
 const paymentRoute = require('./routes/payment');
